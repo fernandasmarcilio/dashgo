d58bbf816d36ae750d4371f0bf62d9fe27b01b81
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-02 15:28:13 -0300

    Create sidebar drawer for mobile version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,51 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react"
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SiderbarDrawerContext";
 
-import { Logo } from './Logo'
-import { NotificationsNav } from './NotificationsNav'
-import { Profile } from './Profile'
-import { SearchBox } from './SearchBox'
+import { Logo } from "./Logo";
+import { NotificationsNav } from "./NotificationsNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideScreenVersion = useBreakpointValue({
     base: false,
-    lg: true
-  })
+    lg: true,
+  });
 
   return (
-    <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      
+      {!isWideScreenVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          aria-label="Abrir menu"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
       <Logo />
 
       {isWideScreenVersion && <SearchBox />}
-      
+
       <Flex align="center" ml="auto">
         <NotificationsNav />
         <Profile showProfileData={isWideScreenVersion} />
       </Flex>
     </Flex>
-  )
+  );
 }
