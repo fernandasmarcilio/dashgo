c7c2108aee5b88d8243a2884fce6a822c71d00af
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-03 16:55:45 -0300

    Add and configure react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,27 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { makeServer } from '../services/mirage'
-import { theme } from '../styles/theme'
+import { AppProps } from "next/app";
+import { makeServer } from "../services/mirage";
+import { theme } from "../styles/theme";
 
-import { SidebarDrawerProvider } from '../contexts/SiderbarDrawerContext'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import { SidebarDrawerProvider } from "../contexts/SiderbarDrawerContext";
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer()
+if (process.env.NODE_ENV === "development") {
+  makeServer();
 }
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import {
   Box,
   Button,
   Checkbox,
   Flex,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -15,6 +17,7 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Heading } from "../../components/Heading";
@@ -22,6 +25,13 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideScreenVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -52,65 +62,80 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuários</Th>
-                {isWideScreenVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Fernanda Marcilio</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      fernandas.marcilio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>
+                Falha ao obter dados dos usuários. Tente novamente mais tarde.
+              </Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuários</Th>
+                    {isWideScreenVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Fernanda Marcilio</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          fernandas.marcilio@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Fernanda Marcilio</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      fernandas.marcilio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Fernanda Marcilio</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          fernandas.marcilio@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Fernanda Marcilio</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      fernandas.marcilio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Fernanda Marcilio</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          fernandas.marcilio@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideScreenVersion && <Td>04 de Abril, 2021</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
+          
         </Box>
       </Flex>
     </Box>
