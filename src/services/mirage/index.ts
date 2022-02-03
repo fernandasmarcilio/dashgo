fe8d7d313be437111b0054adc0785b203612eddc
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-03 16:04:02 -0300

    Add and configure miragejs and fakerjs

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,51 @@
+import { createServer, Factory, Model } from 'miragejs'
+import faker from '@faker-js/faker'
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export function makeServer() {
+  const server = createServer({
+
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+    // fabrica de dados 
+    factories: {
+      user: Factory.extend({
+        name(index) {
+          return faker.name.findName();
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      })
+    },
+
+    // criação dos dados, para o user vai ser gerado 200 usuários
+    seeds(server) {
+      server.createList('user', 200)
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    }
+
+  })
+
+  return server;
+}
