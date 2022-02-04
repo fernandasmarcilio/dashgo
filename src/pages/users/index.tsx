4b691e16caea9b3efe62db2a75eddddfae4b8e1e
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-04 08:44:41 -0300

    Add and configure axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,6 +17,7 @@ import {
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 import { Header } from "../../components/Header";
 import { Heading } from "../../components/Heading";
@@ -25,8 +26,7 @@ import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+    const { data } = await api.get('users')
 
     const users = data.users.map(user => {
       const { id, name, email, createdAt } = user;
